fae132f02947d3dfa7062a095e7fa90bf9866813
dpembert <60945837+dpembert@users.noreply.github.com>
2020-05-02 17:21:23 -0400

    Added Character and horizontal movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+
+
+    }
+
+    void FixedUpdate()
+    {
+        //move the character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
+
