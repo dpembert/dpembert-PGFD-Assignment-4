e3c63175c81a88620f0844c463c70336d2ce9434
dpembert <60945837+dpembert@users.noreply.github.com>
2020-05-03 17:28:31 -0400

    added end trigger

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Check if player collides with pit collider
+        if (collision.tag == "Player")
+        {
+            //Reload the scene if player collides with pit
+            Debug.Log("You Win");
+        }
+    }
+}
+
